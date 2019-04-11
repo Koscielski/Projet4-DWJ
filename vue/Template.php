a60bbd50bf2ecfc35ce543424b128eb2c1b788d5
Koscielski <julienmorot@hotmail.fr>
2019-04-11 13:29:18 +0200

    Mise a jour template

--- vue/Template.php
+++ vue/Template.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <html lang="fr">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="#">
+    <title><?= $title ?>
 <head>
     
 </head>
