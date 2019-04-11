746c56e669309f5bc4d3b6f7fd2c623177528caf
Koscielski <julienmorot@hotmail.fr>
2019-04-11 13:06:02 +0200

    Connexion base de donnée

--- modele/Manager.php
+++ modele/Manager.php
@@ -1,1 +1,10 @@
 <?php
+
+namespace modele;
+
+class Manager{
+  protected function dbConnect(){
+    $db = new PDO('mysql:host=localhost;dbname=alaska;charset=UTF-8', 'root', '');
+    return $db;
+  }
+}
